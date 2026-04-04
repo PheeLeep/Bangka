35bd59ffa91b2b0f4785d4fd2c76226dbdc5d629
PheeLeep <ggprogrammin@gmail.com>
2026-04-04 13:51:16 +0800

    Initialization of bangkactl

---
+++ bangkactl/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace bangkactl;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Hello, World!");
+    }
+}
